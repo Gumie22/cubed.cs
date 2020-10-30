194c020f11bc0f8209d38afaa24ed8ab9e17b937
Gumani Ratshisuka <62070830+Gumie22@users.noreply.github.com>
2020-10-30 15:38:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//This is a program for calculating the cubed number given below
+using System;
+
+namespace program_1.cs
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(cube(5));
+
+            Console.ReadLine();
+        }
+        static int cube(int num)
+        {
+            int result = num * num * num;
+            return result;
+        }
+    }
+}
